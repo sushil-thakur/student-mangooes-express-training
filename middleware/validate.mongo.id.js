f420ac80748521d7c6df0e382f5ffebdb5b0d388
sushil thakur <sushilt2059@gmail.com>
2025-01-24 11:49:48 +0545

    push

---
+++ middleware/validate.mongo.id.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const validateMongoIdFromReqParams = (req, res, next) => {
+    //extract id from req.params
+    const productId = req.params.id;
+
+    //check from mongo id validity
+    const isValidId = mongoose.isValidObjectId(productId);
+
+    //if not valid id, return 400 error
+    if (!isValidId) {
+        return res.status(400).send({ message: "invalid product id" });
+    }
+    next();
+};
+export{validateMongoIdFromReqParams};
--- product/product.controller.js
+++ product/product.controller.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ProductTable from './product.model.js';
 import  productValidationSchema  from './product.validation.js';
 import isStudent from '../middleware/authentication.middleware.js';
+import  validateMongoIdFromReqParams from '../middleware/validate.mongo.id.js';
 
 const router =express.Router();
 
@@ -29,7 +30,6 @@ router.post("/product/add", isStudent,
     return res.status(201).send({message:"product adding...."})
 })
 
-export {router as productController};
 
 //delete product by id
 // router.delete("/product/delete/:id",async(req,res)=>{
@@ -54,7 +54,10 @@ export {router as productController};
 // });
 
 //get product by id
-router.get("/product/:id",async(req,res, next)=>{
+router.get("/product/:id",
+    isStudent,
+    validateMongoIdFromReqParams,
+    async(req,res, next)=>{
     //extract product id from req.params
     const productId = req.params.id;
     const productIDIsvalid = mongoose.isValidObjectId(productId);
@@ -72,5 +75,41 @@ async(req,res)=>{
     if(!product){
         return res.status(404).send({message:"product not found"});
     }
+    await ProductTable
     return res.status(200).send({message:"success",productDetails:product});
 });
+
+
+
+//delete
+router.delete("/product/delete/:id",isStudent, (req,res,next)=>{
+    //extract id from req.params
+    const productId = req.params.id;
+
+    //check if the id is a valid mongodb object id
+    const isValidId = mongoose.isValidObjectId(productId);
+    //if not valid id, return 400 error
+    if(!isValidId){
+        return res.status(400).send({message:"invalid product id"});
+    }
+    next();
+},
+async(req,res)=>{
+    //extract id from req.params
+    const productId = req.params.id;
+    //find product 
+    //const product = await ProductTable.findOne({_id:productId});
+    const product = await ProductTable.findById(productId);
+
+    if(!product){
+        return res.status(404).send({message:"product not found"});
+    }
+
+    await ProductTable.findByIdAndDelete(productId);
+        //await ProductTable.deleteOne({_id:productId});
+        return res.status(200).send({message:"product deleted"});
+    });
+
+    return res.status(200).send({message:"success",productDetails:product});
+
+export {router as productController};
